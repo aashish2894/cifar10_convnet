edfd717431577d51544efa619e7b2190ca88e6ec
aashish2894 <anshu.2894@gmail.com>
2017-11-24 00:44:11 -0600

    Code Added

---
+++ convolution_network_code.py
@@ -0,0 +1,91 @@
+# As usual, a bit of setup
+import numpy as np
+import matplotlib.pyplot as plt
+from cs231n.classifiers.cnn import *
+from cs231n.data_utils import get_CIFAR10_data
+from cs231n.gradient_check import eval_numerical_gradient_array, eval_numerical_gradient
+from cs231n.layers import *
+from cs231n.fast_layers import *
+from cs231n.solver import Solver
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# for auto-reloading external modules
+# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
+
+def rel_error(x, y):
+	""" returns relative error """
+	return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
+data = get_CIFAR10_data()
+for k, v in data.items():
+	print('%s: ' % k, v.shape)
+
+num_train = 100
+small_data = {
+  'X_train': data['X_train'][:num_train],
+  'y_train': data['y_train'][:num_train],
+  'X_val': data['X_val'],
+  'y_val': data['y_val'],
+}
+
+
+
+# model = ThreeLayerConvNet(weight_scale=0.001, hidden_dim=500, reg=0.001)
+
+# solver = Solver(model, data,
+#                 num_epochs=1, batch_size=50,
+#                 update_rule='adam',
+#                 optim_config={
+#                   'learning_rate': 1e-3,
+#                 },
+#                 verbose=True, print_every=20)
+# solver.train()
+
+
+
+# model = ThreeLayerConvNet(use_batchnorm = True)
+
+# N = 50
+# X = np.random.randn(N, 3, 32, 32)
+# y = np.random.randint(10, size=N)
+
+# loss, grads = model.loss(X, y)
+# print('Initial loss (no regularization): ', loss)
+
+# model.reg = 0.5
+# loss, grads = model.loss(X, y)
+# print('Initial loss (with regularization): ', loss)
+
+# num_inputs = 2
+# input_dim = (3, 10, 10)
+# reg = 0.0
+# num_classes = 10
+# X = np.random.randn(num_inputs, *input_dim)
+# y = np.random.randint(num_classes, size=num_inputs)
+
+# model = ThreeLayerConvNet(num_filters=3, filter_size=3,
+#                           input_dim=input_dim, hidden_dim=7,
+#                           dtype=np.float64,use_batchnorm = True)
+# loss, grads = model.loss(X, y)
+
+
+
+
+input_dim = [3, 32, 32]
+
+model = FirstConvNet(num_filters=[16, 32, 64, 128], filter_size=3,
+                          input_dim=input_dim, hidden_dims=[256, 256],
+                          dtype=np.float64,use_batchnorm = True, reg=0.05)
+
+solver = Solver(model, data,
+                num_epochs=2000, batch_size=50,
+                update_rule='adam',
+                optim_config={
+                  'learning_rate': 1e-3,
+                },
+                verbose=True, print_every=20)
+solver.train()
+
